ddba8f1290790ba267a25fb97d2bfb7e96e41576
Grabe,Julia_Cathleen (MED BDS) BIP-DE-B <julia_cathleen.grabe@boehringer-ingelheim.com>
2024-10-25 09:23:01 +0100

    changed colors to symbols for differing the methods

--- R/plot_type_I_error.R
+++ R/plot_type_I_error.R
@@ -2,9 +2,8 @@ plot_type_I_error <- function(data){
 
   plot <- ggplot(data,
                  aes(x = effect_name, y = power, color = method)) +
-    geom_point(position = position_dodge(width = 0.3)) +
-    geom_hline(yintercept = 0, linetype="dashed", color = "darkgrey") +
-    geom_hline(yintercept = 0.025, linetype="dashed", color = "black") +
+    geom_point(position = position_dodge(width = 0.3), aes(shape = method)) +
+    geom_hline(yintercept = 0.025, linetype="dashed", color = "darkgrey") +
     geom_errorbar(aes(
       ymin = lower_ci,
       ymax = upper_ci),
@@ -12,7 +11,8 @@ plot_type_I_error <- function(data){
       position = position_dodge(width = 0.3)) +
     theme_bw() +
     labs(x="effect", y="type I error rate") +
-    scale_color_manual(name = "Method", values = c("truncHochberg" = "black", "Bonferroni" = "darkgray", "parametric" = "gray")) +
+    scale_color_manual(name = "Method", values = c("truncHochberg" = "black", "Bonferroni" = "black", "parametric" = "black")) +
+    scale_shape_manual(name = "Method", values = c("truncHochberg" = 1, "Bonferroni" = 2, "parametric" = 0)) +
     theme(legend.position = "bottom", legend.text = element_text(size = 10),  text = element_text(size = 14)) +
     facet_grid(estimate ~ gamma)
 
